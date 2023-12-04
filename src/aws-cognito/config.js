b8680264d5c73a88734d68e37f28802f8e8f7ce1
Emayan Vadivel <68622989+EMAYAN08@users.noreply.github.com>
2023-12-04 10:10:56 -0400

    Update config.js

--- src/aws-cognito/config.js
+++ src/aws-cognito/config.js
@@ -1,9 +1,9 @@
 // aws-cognito/config.js
 
 const config = {
-      region: 'ca-central-1', // replace with your Cognito region
-      userPoolId: 'ca-central-1_kbPEi2BmN', // replace with your Cognito user pool id
-      clientId: '2k5i8jo6fm5dla3rkv0j5bmql3', // replace with your Cognito client id
+      region: 'YOUR-REGION', // replace with your Cognito region
+      userPoolId: 'YOUR-USER-POOL', // replace with your Cognito user pool id
+      clientId: 'YOUR-CLIENT-ID', // replace with your Cognito client id
     };
     
 export default config;
